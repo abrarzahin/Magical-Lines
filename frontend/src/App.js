23c3c6fc54360e538031dd453725b9566bf071ab
abrar zahin <abrarzahin@yahoo.com>
2019-06-15 17:50:05 -0400

    5 style added CSS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,6 +22,21 @@ class App extends Component {
             <Route path="/articles-list" component={ArticlesList} />
             <Route path="/article" component={ArticlePage} />
           </div>
+          <footer >
+      <div class="collapse bg-dark" id="navbarFooter">
+      <div class="container">
+        <div class="row">        
+          <div class="col-sm-8 col-md-7 py-4">
+          </div>        
+        </div>
+      </div>
+    </div>
+    <div class="navbar navbar-dark bg-dark shadow-sm">
+      <div class="container d-flex justify-content-between">
+          <h6 class="lead text-muted">Abrar Zahin</h6>
+      </div>
+    </div>
+</footer>
         </div>
       </Router>
     );
