de5b68b82ea0b98a9303731c221e440183048da4
abrar zahin <abrarzahin@yahoo.com>
2019-06-15 11:45:53 -0400

    2 homepage compo

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const HomePage = () => (
+    <>
+    <h1>Hello, welcome to Magical Lines!</h1>
+    <p>
+     This site was created to help people learn about application development.
+     While this blog has many posts detailing application development and its components, it’s not designed to be a step-by-step tutorial.
+
+     I designed the blog as a teaching tool first and foremost, but it is designed to be simple, clean, and responsive.
+     I used MERN stack as my starting points and narrowed them down to what I considered to be the essentials.
+     The menu is manually generated but is responsive and built on pure CSS. The HTML focuses on semantics and is built with accessibility in mind.
+     The CSS is not based on any framework, and only contains what the blog needs, there are no layout grids or extra UI classes.
+    </p>
+    
+   
+    </>
+);
+
+export default HomePage;
