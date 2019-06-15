4503925980ea841fdb12bd0a853533ed5078ecca
abrar zahin <abrarzahin@yahoo.com>
2019-06-15 16:21:54 -0400

    4 nav bar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,9 @@ import {
 } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
+import ArticlesList from './pages/ArticlesList';
+import ArticlePage from './pages/ArticlePage';
+import NavBar from './NavBar';
 import './App.css';
 
 class App extends Component {
@@ -12,8 +15,13 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-          <Route path="/" component={HomePage} exact />
-          <Route path="/about" component={AboutPage}  />
+          <NavBar/>
+          <div id="page-body">
+            <Route path="/" component={HomePage} exact />
+            <Route path="/about" component={AboutPage} />
+            <Route path="/articles-list" component={ArticlesList} />
+            <Route path="/article" component={ArticlePage} />
+          </div>
         </div>
       </Router>
     );
