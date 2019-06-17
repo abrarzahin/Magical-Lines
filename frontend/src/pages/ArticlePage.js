d63dd6adf31712877e10959ba820a5961d836144
abrar zahin <abrarzahin@yahoo.com>
2019-06-16 21:08:03 -0400

    25 production ready

--- frontend/src/pages/ArticlePage.js
+++ frontend/src/pages/ArticlePage.js
@@ -1,7 +1,8 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import ArticlesList from '../components/ArticlesList';
 import CommentsList from '../components/CommentsList';
 import UpvotesSection from '../components/UpvotesSection';
+import AddCommentForm from '../components/AddCommentForm';
 import NotFoundPage from './NotFoundPage';
 import articleContent from './article-content';
 
@@ -9,19 +10,18 @@ const ArticlePage = ({ match }) => {
     const name = match.params.name;
     const article = articleContent.find(article => article.name === name);
 
-    const [articleInfo, setArticleInfo] = useState({ upvotes:0,comments:[]});
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
 
     useEffect(() => {
         const fetchData = async () => {
             const result = await fetch(`/api/articles/${name}`);
             const body = await result.json();
-            console.log(body);
             setArticleInfo(body);
         }
         fetchData();
     }, [name]);
 
-    if (!article) return <NotFoundPage/>
+    if (!article) return <NotFoundPage />
 
     const otherArticles = articleContent.filter(article => article.name !== name);
 
@@ -33,9 +33,11 @@ const ArticlePage = ({ match }) => {
             <p key={key}>{paragraph}</p>
         ))}
         <CommentsList comments={articleInfo.comments} />
+        <AddCommentForm articleName={name} setArticleInfo={setArticleInfo} />
         <h3>Other Articles:</h3>
         <ArticlesList articles={otherArticles} />
         </>
     );
 }
+
 export default ArticlePage;
